e25408047d8c7e0ab0028848b41eb0e99853786b
Mateusz Kojro <mateuszkojro@outlook.com>
2021-01-18 23:24:43 +0100

    Zamiennik dla parsera i klasy dla morsa w konsoli do pliku i migajkacego

--- inc/MorseFactory.h
+++ inc/MorseFactory.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <fstream>
 
 /*  Klasa MorseFactory
  *  Autor: Mateusz Kojro
@@ -14,28 +15,91 @@
  *    - Lacze morse_factory z klasa morse z wczesniejszego zadania
  *
  */
+void blink_scroll_lock(unsigned time);
+void press_scroll_lock();
 
 class morseFile : public morse {
 public:
-  virtual void emit(morse_code); // save to file
-  virtual ~morseFile();
+	void set_path(std::string path) {
+		path_ = path;
+	}
+	virtual void emit(morse_code code) final override {
+
+		LOG("Co jak plik nie istnieje");
+		std::fstream file;
+		file.open(this->path_, std::ios::out);
+		file << "freq:" << this->freq_ << std::endl;
+		file << "dot_time:" << this->dot_time_<< std::endl;
+		file << "dash_time:" << this->dash_time_<< std::endl;
+		file << "pause_time:" << this->pause_time_<< std::endl;
+		file << "char_pause:" << this->char_pause_<< std::endl;
+
+		file << code;
+
+		file.close();
+	}
+	virtual ~morseFile() {};
+private:
+	std::string path_;
 };
+
+class morseLight : public morse {
+public:
+	virtual void emit(morse_code code) final override {
+		for (auto znak : code.get_code()) {
+			switch (znak) {
+			case '.':
+				blink_scroll_lock(dot_time_);
+				break;
+			case '-':
+				blink_scroll_lock(dash_time_);
+				break;
+			case '_':
+				Sleep(pause_time_);
+				break;
+			}
+		}
+	}
+	virtual ~morseLight() {};
+private:
+	std::string path_;
+};
+
+class morseTerminal : public morse {
+public:
+	morseTerminal() : morse() {}
+	virtual void emit(morse_code code) final override {
+
+		std::cout << "freq:" << this->freq_<< std::endl;
+		std::cout << "dot_time:" << this->dot_time_<< std::endl;
+		std::cout << "dash_time:" << this->dash_time_<< std::endl;
+		std::cout << "pause_time:" << this->pause_time_<< std::endl;
+		std::cout << "char_pause:" << this->char_pause_<< std::endl;
+
+		std::cout << "code: " << code << std::endl;
+
+	}
+	virtual ~morseTerminal() {};
+private:
+	std::string path_;
+};
+
 
 class MorseFactory {
 public:
-  enum out { BEEP, DISK, CONSOLE, BLINK };
+	enum out { BEEP, DISK, CONSOLE, BLINK };
 
-  MorseFactory(){ WARN("konstruktor nie tworzy obiektu na wskazniku") };
-  MorseFactory(const MorseFactory &) = delete;
-  ~MorseFactory() { delete data_; };
-  const MorseFactory &operator=(const MorseFactory &) = delete;
-  void set_output(out);
-  void set_external_info(std::string);
-  void convert(std::string);
-  void convert(int);
+	MorseFactory() { data_ = new morse; };
+	MorseFactory(const MorseFactory&) = delete;
+	~MorseFactory() { delete data_; };
+	const MorseFactory& operator=(const MorseFactory&) = delete;
+	void set_output(out);
+	void set_external_info(std::string);
+	void convert(std::string);
+	void convert(int);
 
 private:
-  morse *data_;
+	morse* data_;
 };
 
 #endif
