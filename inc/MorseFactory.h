055564123b27aa95ee54d5b989ba824a1b072360
Mateusz Kojro <mateuszkojro@outloook.com>
2021-01-18 02:23:59 +0100

    Morse factory now uses morse

--- inc/MorseFactory.h
+++ inc/MorseFactory.h
@@ -1,41 +1,41 @@
 #ifndef MORSE_FACTORY
 #define MORSE_FACTORY
+#include "debug.h"
+#include "morse.h"
+#include "morse_code.h"
 #include <iostream>
+#include <stack>
+#include <string>
 
 /*  Klasa MorseFactory
  *  Autor: Mateusz Kojro
  *  Data utworzenia:  16.01.2021
  *  Zmiany:
- *    - 
+ *    - Lacze morse_factory z klasa morse z wczesniejszego zadania
  *
  */
-class IMorse {
-  public:
-    IMorse();
-    virtual void convert() = 0;
-};
 
-class MorseAudio : public IMorse {
-  public:
-    MorseAudio(){}
-    virtual void convert() final override;
+class morseFile : public morse {
+public:
+  virtual void emit(morse_code); // save to file
+  virtual ~morseFile();
 };
 
 class MorseFactory {
 public:
-  enum out { BEEP, DISK, CONSOLE };
+  enum out { BEEP, DISK, CONSOLE, BLINK };
 
-  MorseFactory();
-  MorseFactory(const MorseFactory&) = delete;
-  ~MorseFactory() = delete;
-  const MorseFactory& operator=(const MorseFactory&) = delete;
+  MorseFactory(){ WARN("konstruktor nie tworzy obiektu na wskazniku") };
+  MorseFactory(const MorseFactory &) = delete;
+  ~MorseFactory() { delete data_; };
+  const MorseFactory &operator=(const MorseFactory &) = delete;
   void set_output(out);
   void set_external_info(std::string);
   void convert(std::string);
   void convert(int);
 
 private:
-  IMorse* data_;
+  morse *data_;
 };
 
 #endif
